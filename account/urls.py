3eacfd38c3216530a78303ab3cc220cd4d2414c9
bek71x <muydinbaevb@gmail.com>
2025-02-12 21:15:50 +0500

    edit profile

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView,PasswordChangeView,PasswordChangeDoneView
 from django.urls import path
 
 
@@ -9,4 +9,6 @@ urlpatterns =[
     path('logout/', LogoutView.as_view(), name = 'logout'),
     path('profile/', ProfileView.as_view(), name = 'profile'),
     path('profile/edit/', EditProfileView.as_view(), name = 'editprofile'),
+    path('profile/password/edit/', PasswordChangeView.as_view(), name = 'editpassword'),
+    path('profile/password/change_done/', PasswordChangeDoneView.as_view(), name = 'password_change_done'),
 ]
